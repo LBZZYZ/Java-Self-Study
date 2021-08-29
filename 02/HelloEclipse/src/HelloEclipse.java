4b18a2c72ca753110a3d8210c6b62d41fb0c10cd
Bingzhi.Li <280761575@qq.com>
2021-08-29 20:55:57 +0800

    02 Add.

---
+++ 02/HelloEclipse/src/HelloEclipse.java
@@ -0,0 +1,21 @@
+
+public class HelloEclipse {
+
+	private static int a = 10;
+	private static int b = 20;
+	
+	public static void main(String[] args) {
+		
+		// 2.5 题1
+		System.out.println("《Java从入门到精通》");
+		System.out.println("是我学习Java语言的最佳书籍");
+		
+		
+		//2.5 题2
+		System.out.println("a + b = " + (a + b));
+		System.out.println("a - b = " + (a - b));
+		System.out.println("a * b = " + (a * b));
+		System.out.println("a / b = " + (a / b));
+	}
+
+}
