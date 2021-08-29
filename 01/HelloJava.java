641ae2446d34a380cbcc6c5c8fa7fd7921575dce
Bingzhi.Li <280761575@qq.com>
2021-08-29 14:01:41 +0800

    01 Update.

--- 01/HelloJava.java
+++ 01/HelloJava.java
@@ -1,5 +1,16 @@
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+	//例1.1
+        //System.out.println("Hello Java");
+
+	//1.6 题1
+        //System.out.println("这是我的第一个Java程序");
+
+	//1.6 题2
+        System.out.println("----------");
+        System.out.println("|我要学会|");
+        System.out.println("|Java语言|");
+        System.out.println("----------");
+
     }
 }
